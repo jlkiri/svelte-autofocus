0443a9e56813a5db22e7b6a5a05d9cc34a28defb
jlkiri <interalia.dev@gmail.com>
2020-10-04 16:52:49 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+export function autofocus(node, options) {
+  if (!options || !options.visibilityChangeOnly) {
+    node.focus();
+  }
+
+  function focusOnVisibilityChange() {
+    if (document.visibilityState === "visible") {
+      node.focus();
+    }
+  }
+
+  document.addEventListener("visibilitychange", focusOnVisibilityChange);
+
+  return {
+    destroy() {
+      document.removeEventListener("visibilitychange", focusOnVisibilityChange);
+    },
+  };
+}
